8b53f3399eca32f5722d586dc7deebd9d0084a4b
Ravethi <tve16ie049@cet.ac.in>
2025-03-26 14:14:06 +0530

    Initial commit: dbt project setup

--- tests/fct_orders_discount.sql
+++ tests/fct_orders_discount.sql
@@ -1,6 +1,6 @@
-select
-    *
-from
-    {{ref('fct_orders')}}
-where
-    gross_item_sales_amount > 0
+-- select
+--     *
+-- from
+--     {{ref('fct_orders')}}
+-- where
+--     gross_item_sales_amount > 0
